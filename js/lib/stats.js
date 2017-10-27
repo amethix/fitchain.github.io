2f34b715b2e6d1a27e3d5cdaf5f346afc095ad0e
Francesco Gadaleta <frag@frags.local>
2017-10-27 10:49:34 +0200

    added contact form

--- js/lib/stats.js
+++ js/lib/stats.js
@@ -49,6 +49,7 @@ var Stats = function () {
 	msText.innerHTML = 'MS';
 	msDiv.appendChild( msText );
 
+	/*
 	var msGraph = document.createElement( 'div' );
 	msGraph.id = 'msGraph';
 	msGraph.style.cssText = 'position:relative;width:74px;height:30px;background-color:#0f0';
@@ -61,6 +62,7 @@ var Stats = function () {
 		msGraph.appendChild( bar );
 
 	}
+	*/
 
 	var setMode = function ( value ) {
 
@@ -80,67 +82,44 @@ var Stats = function () {
 
 	};
 
-	var updateGraph = function ( dom, value ) {
 
+	var updateGraph = function ( dom, value ) {
 		var child = dom.appendChild( dom.firstChild );
 		child.style.height = value + 'px';
-
 	};
-
 	return {
-
 		REVISION: 12,
-
 		domElement: container,
-
 		setMode: setMode,
-
 		begin: function () {
-
 			startTime = Date.now();
-
 		},
-
 		end: function () {
-
 			var time = Date.now();
-
 			ms = time - startTime;
 			msMin = Math.min( msMin, ms );
 			msMax = Math.max( msMax, ms );
-
 			msText.textContent = ms + ' MS (' + msMin + '-' + msMax + ')';
-			updateGraph( msGraph, Math.min( 30, 30 - ( ms / 200 ) * 30 ) );
-
+			//updateGraph( msGraph, Math.min( 30, 30 - ( ms / 200 ) * 30 ) );
 			frames ++;
-
 			if ( time > prevTime + 1000 ) {
-
 				fps = Math.round( ( frames * 1000 ) / ( time - prevTime ) );
 				fpsMin = Math.min( fpsMin, fps );
 				fpsMax = Math.max( fpsMax, fps );
-
 				fpsText.textContent = fps + ' FPS (' + fpsMin + '-' + fpsMax + ')';
-				updateGraph( fpsGraph, Math.min( 30, 30 - ( fps / 100 ) * 30 ) );
-
+				//updateGraph( fpsGraph, Math.min( 30, 30 - ( fps / 100 ) * 30 ) );
 				prevTime = time;
 				frames = 0;
-
 			}
-
 			return time;
 
 		},
-
 		update: function () {
-
 			startTime = this.end();
-
 		}
-
 	}
-
 };
+
 
 if ( typeof module === 'object' ) {
 
